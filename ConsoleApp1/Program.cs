6f02cf8032413912015455909bbb32896bb7fb73
SergeiKirillov <sergei.kirillov.a@gmail.com>
2020-09-05 14:59:59 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumberYear = (int)DateTime.Now.Year;
+            int NumberMonth = (int)DateTime.Now.Month;
+            int day = (int)DateTime.Now.Day;
+            int NumberMinut = ((int)DateTime.Now.Hour * 60) + ((int)DateTime.Now.Minute);
+
+        }
+    }
+}
